c0583815fd98a0edca6b7adb47986e64b1ebd07a
mas9311 <mas9311@gmail.com>
2018-11-26 21:25:46 -0700

    Completed Breadth-First Search and final print out

--- src/main/java/com/movies/java/sixDegreesOfSeparation.java
+++ src/main/java/com/movies/java/sixDegreesOfSeparation.java
@@ -19,11 +19,9 @@ import java.util.regex.Pattern;
 
 public class sixDegreesOfSeparation {
     public static SparkSession spark;
-    public static final ArrayList<String> EMPTY = new ArrayList<>();
 
     static String crewDataFile  = "src/main/resources/actors";
     static String titleDataFile = "src/main/resources/movies";
-    private static final Pattern COMMA = Pattern.compile(",");
     private static final Pattern TAB = Pattern.compile("\t");
 
     public static String sourceID;
@@ -34,102 +32,6 @@ public class sixDegreesOfSeparation {
 
     public static Set<String> titlesVisited;
     public static Set<String> actorsVisited;
-
-    public static ArrayList<String> getAssociations(String id) {
-        Dataset<Row> row = null;
-        ArrayList<String> associationsList = new ArrayList<>();
-
-        if (id.charAt(0) == 'n') {
-            if (actorsVisited.contains(id)) {
-                return EMPTY; // dead end
-            } else {
-                synchronized (actorsVisited) {
-                    actorsVisited.add(id);
-                }
-            }
-            row = spark.sql("SELECT assoc FROM global_temp.crew_T WHERE id='" + id + "'");
-
-        } else if (id.charAt(0) == 't') {
-            if (titlesVisited.contains(id)) {
-                return EMPTY; // dead end
-            } else {
-                synchronized (titlesVisited) {
-                    titlesVisited.add(id);
-                }
-            }
-            row = spark.sql("SELECT assoc FROM global_temp.title_T WHERE id='" + id + "'");
-        }
-//        System.out.println(id);
-        String[] parts = row.collectAsList().get(0).toString().split("__");
-        String[] associationsArray = parts[1].split(",");
-
-        for (String each : associationsArray) {
-            if (each.charAt(each.length() - 1) == ']') {
-                each = each.substring(0, each.length() - 1);
-            }
-//            System.out.println(id + " is associated to " + each);
-            associationsList.add(each);
-        }
-
-        return associationsList;
-    }
-
-    public static String getCrewID(String name) {
-        Dataset<Row> row = spark.sql("SELECT id FROM global_temp.crew_T WHERE assoc LIKE '" + name + "__%'");
-        Row attributes = row.collectAsList().get(0);
-        return attributes.get(0).toString();
-    }
-
-    public static ArrayList<String> dfs(int depth, String crewID) {
-        if (depth > 6) {
-            return EMPTY; // depth = 7
-        }
-
-//        System.out.println("depth " + depth + ": " + crewID);
-        ArrayList<String> movies = getAssociations(crewID);
-
-        HashMap<String, ArrayList<String>> movieActors = new HashMap<>();
-
-        for (String movieID : movies) {
-            // if (movieID.equals(destinationID)) { path.add(movie); return path; }
-            ArrayList<String> crewAlsoInMovie = getAssociations(movieID);
-
-            for (int i = 0; i < crewAlsoInMovie.size(); ++i) {
-                String currentCrewAlsoInMovie = crewAlsoInMovie.get(i);
-                if (currentCrewAlsoInMovie.equals(destinationID)) {
-                    ArrayList<String> path = new ArrayList<>();
-                    path.add(movieID);
-                    path.add(currentCrewAlsoInMovie);
-                    return path;
-                } else if (currentCrewAlsoInMovie.equals(crewID)) {
-//                    System.out.println("Removing self (" + crewID + ") from " + movieID);
-                    crewAlsoInMovie.remove(i--);
-                }
-            }
-            movieActors.put(movieID, crewAlsoInMovie);
-        }
-
-        for (int i = 0; i < movies.size(); ++i) {
-            String currentMovie = movies.get(i);
-            ArrayList<String> crew = movieActors.get(currentMovie);
-            for (String currentCrew : crew) {
-                ArrayList<String> path = dfs(depth + 1, currentCrew);
-
-                if (!path.isEmpty()) {
-
-                    if (path.size() >= 2) {
-                        path.add(0, currentCrew);
-                        path.add(0, currentMovie);
-                        return path;
-                    }
-                    return path;
-                }
-            }
-            movieActors.remove(currentMovie);
-        }
-
-        return EMPTY; // Did not find
-    }
 
     public static JavaPairRDD<String, String> makeRDD(String whichFile) {
         return spark.read().textFile(whichFile).javaRDD().
@@ -149,6 +51,111 @@ public class sixDegreesOfSeparation {
                 });
     }
 
+    public static String getCrewID(String name) {
+        Dataset<Row> row = spark.sql("SELECT id FROM global_temp.crew_T WHERE assoc LIKE '" + name + "__%'");
+        Row attributes = row.collectAsList().get(0);
+        return attributes.get(0).toString();
+    }
+
+    public static class Node {
+        private Node parent;
+        private String value;
+        private boolean isActor;
+
+        Node(Node parent, String value) {
+            this.parent = parent;
+            this.value = value;
+            this.isActor = (parent == null) || (!parent.isActor);
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+
+        public String extractName(String id) {
+            String tableToUse = (id.charAt(0) == 'n') ? "crew" : "title";
+            Dataset<Row> row = spark.sql("SELECT assoc FROM global_temp." + tableToUse + "_T WHERE id='" + id + "'");
+            String[] attributes = row.collectAsList().get(0).toString().split("__");
+            return attributes[0].substring(1, attributes[0].length());
+        }
+
+        public String toString() {
+            String currentName = extractName(this.value);
+
+            if(parent == null) {
+                return currentName + " was in";
+            }
+            else {
+                if(isActor) {
+                    return parent.toString() + "with " + currentName + "\n" + currentName;
+                } else {
+                    String extra = (parent.parent == null) ? " " : " was in ";
+                    return parent.toString() + extra + currentName + " ";
+                }
+            }
+        }
+
+        public boolean isAnActor() {
+            return this.isActor;
+        }
+
+        public int getDepth() {
+            Node curr = this;
+            int i = 0;
+            while (curr.parent != null) {
+                i++;
+                curr = curr.parent;
+            }
+            return i;
+        }
+    }
+
+
+    public static ArrayList<Node> getChildren(Node parent) {
+        String whichTable = (parent.getValue().charAt(0) == 'n') ? "crew" : "title";
+        Dataset<Row> row = spark.sql("SELECT assoc FROM global_temp." + whichTable + "_T WHERE id='" + parent.getValue() + "'");
+
+        String[] parts = row.collectAsList().get(0).toString().split("__");
+        String[] associationsArray = parts[1].split(",");
+        ArrayList<Node> associationsList = new ArrayList<>();
+
+        for (String each : associationsArray) {
+            if (each.charAt(each.length() - 1) == ']') {
+                each = each.substring(0, each.length() - 1);
+            }
+            associationsList.add(new Node(parent, each));
+        }
+
+        return associationsList;
+    }
+
+    public static Node bfs() {
+        Node root = new Node(null, sourceID);
+
+        int depth = 0;
+        Queue<Node> nodes = new LinkedList<>();
+        actorsVisited.add(root.getValue());
+        nodes.offer(root);
+
+        while(!nodes.isEmpty() && depth < 13) {
+            Node node = nodes.poll();
+            for(Node n : getChildren(node)) {
+                if (n.getValue().equals(destinationID)) {
+                    return n;
+                }
+                if(n.isAnActor() && !actorsVisited.contains(n.getValue())) {
+                    actorsVisited.add(n.getValue());
+                    nodes.add(n);
+                } else if(!n.isAnActor() && !titlesVisited.contains(n.getValue())) {
+                    titlesVisited.add(n.getValue());
+                    nodes.add(n);
+                }
+            }
+            depth++;
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         if(args.length < 2){
             System.out.println("USAGE: sixDegreesOfSeparation <nameOfFromPerson> <nameOfToPerson>");
@@ -166,45 +173,31 @@ public class sixDegreesOfSeparation {
         JavaPairRDD<String, String> crewLines = makeRDD(crewDataFile);
         JavaPairRDD<String, String> titleLines = makeRDD(titleDataFile);
 
-        crewLines.foreach(
-                s -> {
-                    System.out.println(s);
-                }
-        );
-
-        titleLines.foreach(
-                s -> {
-                    System.out.println(s);
-                }
-        );
-
         crewTable = spark.createDataset(crewLines.collect(), Encoders.tuple(Encoders.STRING(), Encoders.STRING())).toDF("id","assoc");
         crewTable.createOrReplaceGlobalTempView("crew_T");
-//        crewTable.show();
 
         titleTable = spark.createDataset(titleLines.collect(), Encoders.tuple(Encoders.STRING(), Encoders.STRING())).toDF("id","assoc");
         titleTable.createOrReplaceGlobalTempView("title_T");
-//        titleTable.show();
 
         sourceID = getCrewID(args[0]);
         destinationID = getCrewID(args[1]);
-        System.out.println("Finding path from " + sourceID + " to " + destinationID);
 
-        ArrayList<String> path = dfs(1, sourceID);
-
-        if (path.isEmpty()) {
-            System.out.println("Did not find a path ]:");
-        } else {
-            System.out.println("Found the path in " + path.size() + " vertices (including movies).");
-            String previous = sourceID;
-            for (String each : path) {
-                System.out.println(each);
-            }
-
-            for (int i = 0; i < path.size(); ++i) {
-                System.out.println(previous + " was in " + path.get(i) + " with " + path.get(++i));
-                previous = path.get(i);
-            }
+        // Run BFS
+        Node path = bfs();
+        if(path != null) {
+            System.out.println(path.toString() + " was found in " + path.getDepth()/2 + " people!");
+        }else {
+            System.out.println("Cant find path");
         }
+
+//        Run DFS
+//        ArrayList<String> path = dfs(1, sourceID);
+//        System.out.println("Found the path in " + path.size() + " vertices (including movies).");
+//        String previous = sourceID;
+//
+//        for (int i = 0; i < path.size(); ++i) {
+//            System.out.println(previous + " was in " + path.get(i) + " with " + path.get(++i));
+//            previous = path.get(i);
+//        }
     }
 }
